8ec19aa873888a223bf4e41cfa85934bb11e8e70
Ruben Aguilera <raguilera@autentia.com>
2018-07-15 22:58:10 +0200

    Enable CORS

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { ApplicationModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(ApplicationModule);
+	app.enableCors({
+		origin: '*'
+	});
 	const port: number = parseInt(process.env.PORT) || 3001;
 	await app.listen(port);
 }
